f535faeaebbfd44502cbf49d228db5c95e83cbcd
chandu1308 <98881848+chandu1308@users.noreply.github.com>
2022-08-28 15:45:14 +0530

    Web Resume Changes Done

--- public/script.js
+++ public/script.js
@@ -1,12 +1,17 @@
 (function() {
     const showDate = document.querySelector('.date');
-
-    var today = new Date().toLocaleString();
-    showDate.innerHTML = today;
+    // var today = new Date().toLocaleString();
+    // showDate.innerHTML = today;
     
+    setInterval(myTimer, 1000);
+    function myTimer() {
+        const date = new Date();
+        showDate.innerHTML = date.toLocaleString();
+    }
+
     document.querySelector(".download-btn").addEventListener("click", function() {
         const anchorTag = document.createElement("a");
-        anchorTag.href = "./assets/Chandrasekhar Tirumalasetty_Resume.pdf";
+        anchorTag.href = "./assets/Chandrasekhar_Tirumalasetty_Resume.pdf";
         anchorTag.download = "Chandrasekhar-Tirumalasetty.pdf";
         anchorTag.click();
     });
